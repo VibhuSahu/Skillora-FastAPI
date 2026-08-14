148066294298844e60190609b2df0e1bc0640051
VIBHU SAHU <vibhu6751@gmail.com>
2026-08-14 19:34:47 +0530

    Introduction of Model

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
+# Models
+class User(BaseModel):
+    username: str
+    comment: str
 
+
+
+# Routers
 @app.get("/")
 async def root() -> str:
     return "Hello World Hide"
@@ -12,3 +20,10 @@ async def root() -> str:
 @app.post("/")
 async def response_my_name(payload: dict = Body(...)) -> str:
     return f"Hi! it {payload["name"]}"
+
+@app.post("/user_comment")
+async def user_comment(UserData: User):
+    return{
+        "Your Name": UserData.username,
+        "Your Comment": UserData.comment
+    }
