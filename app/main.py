7a0890c593e3e653abc131141a468abe1515211c
VIBHU SAHU <vibhu6751@gmail.com>
2026-08-14 19:46:02 +0530

    Introduction of Model

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel, EmailStr
 
 
 app = FastAPI()
+
+
+class User(BaseModel):
+    username: str
+    mail: EmailStr
+    rating: int
+
+
 
 
 
@@ -10,5 +19,10 @@ async def root() -> str:
     return "Hello World Hide"
 
 @app.post("/")
-async def response_my_name(payload: dict = Body(...)) -> str:
-    return f"Hi! it {payload["name"]}"
+async def response_my_name(payload: User) -> str:
+    return f"Hi! it {payload.username} and your eamil is {payload.mail} you rated {payload.rating} star to the Course"
+
+
+@app.post("/Hi")
+async def user_comment(payload: dict = Body(...)) -> str:
+    return f"Hi! it {payload["name"]}."
